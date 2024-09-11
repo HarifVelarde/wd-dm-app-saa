1f460a6fea4fcf1fc566d417aa621c93c0716eb6
HarifVelarde <harif.velarde@gmail.com>
2024-09-10 22:56:19 -0600

    adding .tf files

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "annysahbucket" {
+  bucket = "annysah-terraform-practice"
+}
