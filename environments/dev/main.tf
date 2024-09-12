ba1a056d1c8b04d6543825b1b9748e252e7777f7
HarifVelarde <harif.velarde@gmail.com>
2024-09-11 20:41:53 -0600

    adding new bucket

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,3 +19,7 @@ resource "aws_s3_bucket" "annysahbucket" {
 resource "aws_s3_bucket" "testbucket" {
   bucket = "annysah-terraform-test1"
 }
+
+resource "aws_s3_bucket" "wd-bucket" {
+  bucket = "data-management"
+}
