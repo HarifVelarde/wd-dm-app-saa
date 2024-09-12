eba4a4a78d554d64476744cd46ad11d54ddfc8ef
HarifVelarde <harif.velarde@gmail.com>
2024-09-12 17:28:19 -0600

    fixing main access

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "wd-bucket-storage-back" {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "wd-bucket-storage-back" {
+  bucket = aws_s3_bucket.wd-bucket-storage-back.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "wd-bucket-storage-back" {
   bucket = aws_s3_bucket.wd-bucket-storage-back.id
   policy = jsonencode({
