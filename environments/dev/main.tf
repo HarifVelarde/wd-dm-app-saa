dada4ffbd34d08ac4e4685b758e7ea4e6898a323
HarifVelarde <harif.velarde@gmail.com>
2024-09-12 18:09:00 -0600

    removing policy

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -26,24 +26,3 @@ resource "aws_s3_bucket_acl" "wd-bucket-acl" {
   bucket = aws_s3_bucket.wd-bucket-storage-back.id
   acl = "public-read"
 }
-
-resource "aws_s3_bucket_policy" "wd-bucket-storage-back" {
-  bucket = aws_s3_bucket.wd-bucket-storage-back.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal ={
-          AWS = "*"
-        }
-        Action = [
-          "s3:*"
-        ]
-        Resource = [
-          "arn:aws:s3:::data-management-flash/*",
-        ]
-      }
-    ]
-  })
-}
