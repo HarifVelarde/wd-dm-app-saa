2303e0508103c6a47508c5f68fcd7899df0f6079
HarifVelarde <harif.velarde@gmail.com>
2024-09-12 18:07:11 -0600

    fixing main access

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,14 +1,30 @@
 resource "aws_s3_bucket" "wd-bucket-storage-back" {
   bucket = "data-management-flash"
+}
 
+resource "aws_s3_bucket_ownership_controls" "wd-bucket-controls" {
+  bucket = aws_s3_bucket.wd-bucket-storage-back.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "wd-bucket-storage-back" {
   bucket = aws_s3_bucket.wd-bucket-storage-back.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "wd-bucket-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.wd-bucket-controls,
+    aws_s3_bucket_public_access_block.wd-bucket-storage-back
+  ]
+
+  bucket = aws_s3_bucket.wd-bucket-storage-back.id
+  acl = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "wd-bucket-storage-back" {
